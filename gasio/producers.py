36dc46f911aeb1cb8b4bbe4ec1ce306e246a867e
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-08-12 22:48:09 +0200

    Fix producers for new abc design

--- gasio/producers.py
+++ gasio/producers.py
@@ -1,4 +1,6 @@
 import builtins
+import math
+from typing import Sequence, AsyncIterable
 
 import trio
 from async_generator import aclosing
@@ -7,19 +9,17 @@ from .abc import Producer, Refiner
 
 class Extension(Producer):
     def __init__(self, output):
-        self.output = output
+        self._output = output
     
     async def run(self):
         pass
 
 
 class Zip(Producer):
-    def __init__(self, *sources):
-        self.sources = sources
+    def __init__(self, *sources: Sequence[AsyncIterable]):
+        super().__init__()
 
-        send_channel, receive_channel = trio.open_memory_channel(0)
-        self._output_send_channel = send_channel
-        self.output = receive_channel
+        self.sources = sources
 
     async def run(self):
         async with trio.open_nursery() as nursery:
@@ -36,22 +36,22 @@ class Zip(Producer):
             for i, s in builtins.enumerate(self.sources):
                 nursery.start_soon(pull_task, s, results[i][0], pull_controls[i][1])
             while True:
-                await self._output_send_channel.send(tuple([await r.receive() for _, r in results]))
+                await self._send_output_channel.send(tuple([await r.receive() for _, r in results]))
                 for p, _ in pull_controls:
                     await p.send(None)
 
 class Delay(Refiner):
-
     def __init__(self, interval: float):
-        self.input = None
-        buffer_input_channel, buffer_output_channel = trio.open_memory_channel(math.inf)
-
+        super().__init__()
+        self.interval = interval
     
     async def run(self):
+        buffer_input_channel, buffer_output_channel = trio.open_memory_channel(math.inf)
+
         async def pull_task():
-            async with input:
-                async for item in input:
-                    await buffer_input_channel.send((item, trio.current_time() + interval))
+            async with self._input:
+                async for item in self._input:
+                    await buffer_input_channel.send((item, trio.current_time() + self.interval))
         
         async def push_task():
             async with buffer_output_channel:
@@ -59,8 +59,8 @@ class Delay(Refiner):
                     now = trio.current_time()
                     if timestamp > now:
                         await trio.sleep(timestamp - now)
-                    await send_channel.send(item)
+                    await self._send_output_channel.send(item)
 
-
-        self.nursery.start_soon(pull_task)
-        self.nursery.start_soon(push_task)
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(pull_task)
+            nursery.start_soon(push_task)
