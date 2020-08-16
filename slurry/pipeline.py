e1ca934280351c8d343fa046f75ec94b1654fc95
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-08-16 20:06:34 +0200

    Add docstrings

--- slurry/pipeline.py
+++ slurry/pipeline.py
@@ -2,6 +2,7 @@
 __version__ = '0.1.0'
 
 from itertools import chain
+import math
 from typing import Sequence
 
 import trio
@@ -11,6 +12,24 @@ from .abc import Section
 from .tap import Tap
 
 class Pipeline:
+    """The ``Pipeline`` is a composable a stream processor. It consists of a chain of
+    ``Sections``, which each handle a single stream processing operation.
+
+    The stream processing results are accessed by calling tap to create an output channel.
+    Each pipeline can have multiple open tabs, each receiving a copy of the output stream.
+    
+    The pipeline can also be extended dynamically with new pipeline sections adding
+    additional processing.
+
+    Note:
+        Do not instantiate a ``Pipeline`` manually. Use ``Pipeline.create`` instead. It
+        returns an async context manager which manages the pipeline lifetime.
+
+    Args:
+        *sections (Sequence[Section]): One or more pipeline sections.
+        main_nursery (trio.Nursery): Nursery used to run data pumping tasks.
+        main_switch (trio.Event): Event to control pipeline start.
+    """
     def __init__(self, *sections: Sequence[Section],
                  main_nursery: trio.Nursery,
                  main_switch: trio.Event):
@@ -22,6 +41,11 @@ class Pipeline:
     @classmethod
     @asynccontextmanager
     async def create(cls, *sections: Sequence[Section]):
+        """Creates a new pipeline context and adds the given section sequence to it.
+
+        Args:
+            *sections (Sequence[Section]): One or more pipeline sections.
+        """
         async with trio.open_nursery() as nursery:
             pipeline = cls(*sections, main_nursery=nursery, main_switch=trio.Event())
             nursery.start_soon(pipeline._pump) # pylint: disable=protected-access
@@ -29,6 +53,7 @@ class Pipeline:
             nursery.cancel_scope.cancel()
 
     async def _pump(self):
+        """Runs the pipeline."""
         await self._main_switch.wait()
 
         if isinstance(self.sections[0], Section):
@@ -58,10 +83,29 @@ class Pipeline:
 
     def tap(self, *,
             max_buffer_size: int = 0,
-            timeout: float = 1,
-            retrys: int = 3,
+            timeout: float = math.inf,
+            retrys: int = 0,
             start: bool = True) -> trio.MemoryReceiveChannel:
-        """Create a new output channel for this pipeline."""
+        """Create a new output channel for this pipeline.
+
+        Multiple channels can be opened and will receive a copy of the output data.
+
+        Note:
+            The output is sent by reference, so if a consumer changes it, other consumers
+            will see the changed output.
+
+        Args:
+            max_buffer_size (int): Although not recommended in general, it is possible to
+                set a buffer on the output channel. (default ``0``)
+            timeout (float): Timeout in seconds when attempting to send an item.
+                (default ``math.inf``)
+            retrys (int): Number of times to retry sending, if the initial attempt fails.
+                (default ``0``)
+            start (bool): Start processesing when opening this tap. (default ``True``)
+
+        Returns:
+            trio.MemoryReceiveChannel
+        """
         send_channel, receive_channel = trio.open_memory_channel(max_buffer_size)
         self._taps.add(Tap(send_channel, timeout, retrys))
         if start:
@@ -69,7 +113,14 @@ class Pipeline:
         return receive_channel
 
     def extend(self, *sections: Sequence[Section]) -> "Pipeline":
-        """Extend this pipeline into a new pipeline."""
+        """Extend this pipeline into a new pipeline.
+
+        Note:
+            Extending a pipeline implicitly enables it.
+
+        Args:
+            *sections (Sequence[Section]): One or more pipeline sections.
+        """
         pipeline = Pipeline(
             self.tap(),
             sections,
--- slurry/tap.py
+++ slurry/tap.py
@@ -1,12 +1,23 @@
+"""Pipeline output tap."""
 import trio
 
 class Tap:
+    """The tap class is responsible for transmitting the output of the pipeline to consumers.
+    It implements an asynchronous send function that is launched as a task from the pipeline for
+    each item processed. Each tap can be individually configured with send timeouts and retry
+    attempts.
+    """
     def __init__(self, send_channel, timeout, retrys):
         self.send_channel = send_channel
         self.timeout = timeout
         self.retrys = retrys
 
     async def send(self, item):
+        """Handles the transmission of a single item from the pipeline.
+
+        Each send operation is run as a task, so that in case of multiple consumers, a stuck
+        consumer won't block the entire send loop.
+        """
         for _ in range(self.retrys + 1):
             with trio.move_on_after(self.timeout):
                 await self.send_channel.send(item)
