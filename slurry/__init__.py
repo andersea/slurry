b32798db0baf7e72884f6515834e46001c574d21
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-08-23 17:07:50 +0200

    Add missing export

--- slurry/__init__.py
+++ slurry/__init__.py
@@ -7,4 +7,5 @@ from .abc import Section
 from .buffers import Window, Group, Delay, RateLimit
 from .combiners import Chain, Merge, Zip, ZipLatest
 from .filters import Skip, Filter, Changes
+from .producers import Repeat
 from .refiners import Map
