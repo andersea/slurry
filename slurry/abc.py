f8ce6b91d4c83e3b145040323eda943b7815f2c2
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-12-05 14:23:55 +0100

    New resource management documentation

--- slurry/abc.py
+++ slurry/abc.py
@@ -17,19 +17,17 @@ class Section(ABC):
         """The pump method must contain the logic that iterates the input, processes the indidual
         items, and feeds results to the output.
 
+        By default, the pipeline tries to manage the input and output resource lifetime. Normally
+        you don't have to worry about closing the input and output after use. The exception is, if
+        your custom section adds additional input sources, or provides it's own input. In this case
+        the section must take care of closing the input after use.
+
         .. note::
-            When implementing sections, it is important to gracefully handle channel closures. When
-            the upstream iterable is exhausted, close the downstream channel, and vice versa if the
-            downstream channel is closed, propagate the closure upstream. The easiest way to do this
-            is usually to use a context manager.
-
-            Example:
-
-            .. code-block:: python
-
-                async with aclosing(input) as aiter, output:
-                    # .. process input and send to output
-
+            The receiving end of the output can be closed by the pipeline or by the downstream
+            section at any time. If you try to send an item to an output that has a closed receiver,
+            a ``BrokenResourceError`` will be raised. The pipeline knows about this and is prepared
+            to handle it for you, but if you need to do some kind of cleanup, like closing network
+            connections for instance, you may want to handle this exception yourself.
 
         :param input: The input data feed. Will be ``None`` for the first ``Section``, as the first
             ``Section`` is expected to supply it's own input.
