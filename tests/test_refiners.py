cae15819edd9f375f2ac767a90774dc2dbb7ce26
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-12-25 21:58:57 +0100

    Remove test for now unsupported Map functinality

--- tests/test_refiners.py
+++ tests/test_refiners.py
@@ -10,10 +10,3 @@ async def test_map(autojump_clock):
     ) as pipeline, pipeline.tap() as aiter:
         result = [i async for i in aiter]
         assert result == [0, 1, 4, 9, 16]
-
-async def test_map_section(autojump_clock):
-    async with Pipeline.create(
-        Map(lambda x: x*x, Delay(1, produce_increasing_integers(1, max=5)))
-    ) as pipeline, pipeline.tap() as aiter:
-        result = [(i, trio.current_time()) async for i in aiter]
-        assert result == [(0, 1), (1, 2), (4, 3), (9, 4), (16, 5)]
