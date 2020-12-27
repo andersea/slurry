459d48244a80233fd81c02bf1dccca97f5d2bcfe
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-12-27 11:46:05 +0100

    Bump version: 0.9.0 → 0.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Anders Ellenshøj Andersen'
 author = 'Anders Ellenshøj Andersen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.10.0'
 
 
 # -- General configuration ---------------------------------------------------
--- slurry/__init__.py
+++ slurry/__init__.py
@@ -1,4 +1,4 @@
 """An async streaming data processing framework."""
-__version__ = '0.9.0'
+__version__ = '0.10.0'
 
 from .pipeline import Pipeline
