b35761633b2cd027c0ae4bbf1f200d6385fcd525
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2021-12-14 20:48:20 +0100

    Consistent closure behaviour

--- slurry/sections/_producers.py
+++ slurry/sections/_producers.py
@@ -13,6 +13,8 @@ class Repeat(TrioSection):
     If used as a middle section, the input can be used to set the value that is sent. When
     an input is received, it is sent immidiately, and the internal timer resets.
 
+    When an input is used, closure of the input stream will cause the repeater to close as well.
+
     :param interval: Delay between each transmission.
     :type interval: float
     :param default: Default item to send. If not supplied, will wait for an input.
@@ -57,6 +59,7 @@ class Repeat(TrioSection):
                         if running_repeater:
                             running_repeater.cancel()
                         running_repeater = await nursery.start(repeater, item)
+                nursery.cancel_scope.cancel()
 
 class Metronome(TrioSection):
     """Yields an item repeatedly at wall clock intervals.
@@ -65,6 +68,8 @@ class Metronome(TrioSection):
     an input is received, it is stored and send at the next tick of the clock. If multiple
     inputs are received during a tick, only the latest is sent. The preceeding inputs are
     dropped.
+
+    When an input is used, closure of the input stream will cause the metronome to close as well.
 
     :param interval: Wall clock delay between each transmission.
     :type interval: float
@@ -91,19 +96,21 @@ class Metronome(TrioSection):
 
         item = self.default if self.has_default else None
 
-        async def pull_task(task_status=trio.TASK_STATUS_IGNORED):
+        async def pull_task(cancel_scope, *, task_status=trio.TASK_STATUS_IGNORED):
             nonlocal item
             async for item in input:
                 task_status.started()
                 break
             async for item in input:
                 pass
+            cancel_scope.cancel()
 
         async with trio.open_nursery() as nursery:
-            if self.has_default:
-                nursery.start_soon(pull_task)
-            else:
-                await nursery.start(pull_task)
+            if input:
+                if self.has_default:
+                    nursery.start_soon(pull_task, nursery.cancel_scope)
+                else:
+                    await nursery.start(pull_task, nursery.cancel_scope)
             while True:
                 await trio.sleep(self.interval - time() % self.interval)
                 await output(item)
