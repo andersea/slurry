2c2aeda6bfa116f34ae7053b093adf94374f308b
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2021-12-14 20:55:15 +0100

    Bump version: 1.1.1 → 1.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Anders Ellenshøj Andersen'
 author = 'Anders Ellenshøj Andersen'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
