24e14defea9a802d6088453eea3e9d5c3f887021
Mike Nerone <mike@nerone.org>
2024-01-03 16:08:31 -0600

    Bugfix: not all iterables are their own iterators

--- tests/test_threading.py
+++ tests/test_threading.py
@@ -2,11 +2,19 @@ import pytest
 from slurry import Pipeline
 from slurry.sections import Map
 
-from .fixtures import produce_increasing_integers, SyncSquares
+from .fixtures import AsyncNonIteratorIterable, produce_increasing_integers, SyncSquares
 
 async def test_thread_section(autojump_clock):
     async with Pipeline.create(
         produce_increasing_integers(1, max=5),
+        SyncSquares()
+    ) as pipeline, pipeline.tap() as aiter:
+        result = [i async for i in aiter]
+        assert result == [0, 1, 4, 9, 16]
+
+async def test_thread_section_input_non_iterator_iterable(autojump_clock):
+    async with Pipeline.create(
+        AsyncNonIteratorIterable(produce_increasing_integers(1, max=5)),
         SyncSquares()
     ) as pipeline, pipeline.tap() as aiter:
         result = [i async for i in aiter]
