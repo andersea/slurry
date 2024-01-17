b5eefacd17de366b249eccc0a52d3e60286023ed
Mike Nerone <mike@nerone.org>
2024-01-17 01:32:55 -0600

    Redundant symbol imports signal public availability to type checkers

--- slurry/environments/__init__.py
+++ slurry/environments/__init__.py
@@ -1,3 +1,3 @@
-from ._trio import TrioSection
-from ._threading import ThreadSection
-from ._multiprocessing import ProcessSection
+from ._trio import TrioSection as TrioSection
+from ._threading import ThreadSection as ThreadSection
+from ._multiprocessing import ProcessSection as ProcessSection
--- slurry/sections/__init__.py
+++ slurry/sections/__init__.py
@@ -1,6 +1,6 @@
 """A collection of common stream operations."""
-from ._buffers import Window, Group, Delay
-from ._combiners import Chain, Merge, Zip, ZipLatest
-from ._filters import Skip, SkipWhile, Filter, Changes, RateLimit
-from ._producers import Repeat, Metronome, InsertValue
-from ._refiners import Map
+from ._buffers import Window as Window, Group as Group, Delay as Delay
+from ._combiners import Chain as Chain, Merge as Merge, Zip as Zip, ZipLatest as ZipLatest
+from ._filters import Skip as Skip, SkipWhile as SkipWhile, Filter as Filter, Changes as Changes, RateLimit as RateLimit
+from ._producers import Repeat as Repeat, Metronome as Metronome, InsertValue as InsertValue
+from ._refiners import Map as Map
